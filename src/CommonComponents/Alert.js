4206bf4311a77778f8d6645628ddc01e1281b6a8
ajagani@bunnings.com.au <ajagani@bunnings.com.au>
2024-10-21 10:55:29 +1100

    Removed test comment

--- src/CommonComponents/Alert.js
+++ src/CommonComponents/Alert.js
@@ -3,8 +3,6 @@ import Stack from '@mui/material/Stack';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 
-
-//comment
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
